4cdfa23a78b621f0171aec12c0ba46d9e43aae92
joelmirsiqueira <136918049+joelmirsiqueira@users.noreply.github.com>
2025-11-03 12:59:39 -0300

    feat: cricao de funcoes no game-over.js e elementos no game-over.html

--- js/game-over.js
+++ js/game-over.js
@@ -1,8 +1,41 @@
+
+const resultados = sessionStorage.getItem('resultados');
+if (resultados === null) {
+    // window.location.href = 'index.html';
+}
+// sessionStorage.removeItem('resultados');
+const listaResultados = JSON.parse(resultados);
+
 window.addEventListener('load', main)
+
 
 function main() {
     // atualizarRecorde();
     sair();
+}
+
+function processarResultados() {
+    if (listaResultados.length === 1) {
+        atualizarMensagem('game over')
+    } else {
+        listaResultados.sort((a, b) => b.pontos - a.pontos)
+        const vencedor = listaResultados[0];
+        exibirResultados();
+    }
+}
+
+function exibirResultados() {
+    const lista = document.getElementById('lista-result');
+    listaResultados.forEach(resultado => {
+        const item = document.createElement('li');
+        item.textContent = `${resultado.nome} => ${resultado.pontos}`;
+        lista.appendChild(item);
+    });
+}
+
+function atualizarMensagem(texto) {
+    const mensagem = document.getElementById('mensagem');
+    mensagem.textContent = texto.toUpperCase();
 }
 
 function atualizarRecorde() {
